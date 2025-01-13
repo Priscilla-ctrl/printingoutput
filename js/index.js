a017982a30442dbac2232623715c875ba27aedca
Priscilla-ctrl <sam00027@algonquinlive.com>
2025-01-13 12:31:17 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,43 @@
+//TASK 1
+let a = 3;
+let b = 5;
+let c;
+let output = 
+    `
+        let a = 3;
+        let b = 5;
+        let c ;
+        ---------
+        a + b = ${a + b}
+        a - b = ${a - b}
+        a * b = ${a * b}
+        a / b = ${a / b}
+        a % b = ${a % b}
+        a += b = ${a += b}
+        a -= b = ${a -= b}
+        a *= b = ${a *= b}
+        a /= b = ${a /= b}
+        a %= b = ${a %= b}
+        a == b = ${a == b}
+        a != b = ${a != b}
+        a > b = ${a > b}
+        a < b = ${a < b}
+        !a && !c = ${!a && !c}
+        !a || !c = ${!a || !c}
+    `
+
+console.log(output);
+
+
+//TASK 2
+let first_name = "Priscilla";
+let last_name = "Sam";
+let email = "sam00027@algonquin.com";
+let output2;
+
+output2 = 
+    `
+        My name is ${first_name} ${last_name}. You can contact me at ${email}
+    `;
+
+console.log(output2);
